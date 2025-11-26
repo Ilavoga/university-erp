d5f375a2bdb9029be14c1f2591781cbca15350b8
Ilavoga <skisambo@kabarak.ac.ke>
2025-11-26 10:46:59 +0300

    feat: update next.config.js to include remote image patterns for Supabase and Unsplash

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["better-sqlite3"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bwnjplrvmxmdixjwnvgd.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatar.vercel.sh",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
